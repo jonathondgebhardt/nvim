d0c1424c99b41190ec5e2ec0becc6f63d21b4225
Jonathon Gebhardt <gebhardt_jonathon@bah.com>
2025-01-02 14:43:57 -0500

    Italicize comments.

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,14 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'monekai' }
+M.ui = {
+	theme = 'monekai',
+	hl_override = {
+		Comment = { italic = true },
+		["@comment"] = { italic = true },
+	},
+}
 M.plugins = "custom.plugins"
+
 return M
 
