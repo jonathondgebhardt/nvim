3d766963a75868daf0f90df0e6a3f9e102b98351
Jonathon Gebhardt <jonathon.gebhardt@gmail.com>
2025-01-02 19:50:03 -0500

    Always use tabs equivalent to four spaces.

--- lua/options.lua
+++ lua/options.lua
@@ -6,6 +6,11 @@ vim.o.cursorlineopt = "both" -- to enable cursorline!
 vim.o.scrolloff = 10
 vim.o.guifont = "SauceCodePro Nerd Font:h12"
 
+vim.o.autoindent = true
+vim.o.expandtab = false
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
 if vim.g.neovide then
   vim.g.neovide_hide_mouse_when_typing = true
   -- vim.g.neovide_cursor_animation_length = 0.13
